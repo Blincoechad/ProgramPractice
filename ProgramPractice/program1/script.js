17e5a3f04beb76712b62612481c1b8be6d623d41
blincoechad <blincoechad@gmail.com>
2025-08-29 16:37:58 -0400

    First push  BikeSpecs web application

---
+++ ProgramPractice/program1/script.js
@@ -0,0 +1,45 @@
+const bike = {
+    color: 'Green and black',
+    miles: 12000,
+    year: 2022,
+    make: 'Kawasaki',
+    model: 'Ninja 400'
+
+}
+const chad = {
+    name: 'Chad',
+    age: 32,
+    occupation: 'Web Designer/Developer',
+    location: 'Florida',
+    experience: `Ive been doing many course online for Web Designing and Development for a little over a year
+    now. I began school beginning of last year to earn my college certificate
+    so I be well profient in as many areas of Web Development as possible. I continue to grow my skills
+    and learn new things as often as I can. Ive have learned alot involving Git and Github, and the
+    command line commands to push, pull, add, edit and so on. `
+}
+function getBikeSpecs() {
+    const bikeSpecs = `
+        <h2>Bike Specifications</h2>
+        <p><strong>Color:</strong> ${bike.color}</p>
+        <p><strong>Miles:</strong> ${bike.miles}</p>
+        <p><strong>Year:</strong> ${bike.year}</p>
+        <p><strong>Make:</strong> ${bike.make}</p>
+        <p><strong>Model:</strong> ${bike.model}</p>
+    `;
+    document.querySelector('.container').innerHTML = bikeSpecs;
+   ;
+
+}
+const getMyInfo = () => {
+    const myInfo = `
+        <h1>Chad Blincoe</h1>
+        <p><strong>Age:</strong> ${chad.age}</p>
+        <p><strong>Occupation:</strong> ${chad.occupation}</p>
+        <p><strong>Location:</strong> ${chad.location}</p>
+        <p><strong>Experience:</strong> ${chad.experience}</p>
+        <a href="./media/123_1.jpeg" title="View Bike Image">View Bike Image</a>
+    `;
+
+        document.querySelector('.about-me-container').innerHTML = myInfo;
+
+}
